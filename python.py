7d17b0cd72768fe284db0d28bfca4645b27969a6
Kavya224 <marathesaikavya224@gmail.com>
2026-04-14 17:30:11 +0530

    Update python.py

--- python.py
+++ python.py
@@ -147,9 +147,13 @@ def view_equivalence(): return render_template('equivalence.html')
 @app.route('/partial_order')
 def view_partial_order(): return render_template('partial_order.html')
 
+
 # --- 6. SERVER START ---
 # --- 6. SERVER START ---
-if __name__ == '__main__':
-    # Render needs to pick the port itself, this code allows that
+# This version works for both Render and Vercel
+if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-   # app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port)
+
+# Vercel specifically needs this line to find the 'app' object
+app = app
